233f14c96af01f5eb5de0794c25ba5770f492e3f
nm19000 <nmgottschling@gmail.com>
2025-09-15 14:26:47 -0400

    moved RS SR utils to SR example

--- src/accuracy_bounds/inverseproblems/__init__.py
+++ src/accuracy_bounds/inverseproblems/__init__.py
@@ -22,5 +22,17 @@ from .kersize_compute import (
 
 __all__ = (
     "projection_nullspace",
+    "wc_kernelsize_nosym_batch_cuda", 
+    "wc_kernelsize_sym_batch_cuda",
+    "wc_kernelsize_nosym_perbatch_cuda", 
+    "wc_kernelsize_nosym_crossbatch_cuda",
+    "av_kernelsize",
+    "wc_kernelsize",
+    "diams_feasibleset_inv_sym",
+    "diams_feasibleset_inv_sym",
+    "diams_feasibleset_inv",
+    "compute_av_kernel_size",
+    "compute_wc_kernel_size",
+    "compute_feasible_set_linear_forwardmodel",
 )
 
