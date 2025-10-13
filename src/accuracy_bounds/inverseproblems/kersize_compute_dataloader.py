f157b4a4560a8bf9c0cb127aeb91d4330b219fbb
nm19000 <nmgottschling@gmail.com>
2025-10-13 10:57:24 -0400

    edit average kernel size function names and docstrings

--- src/accuracy_bounds/inverseproblems/kersize_compute_dataloader.py
+++ src/accuracy_bounds/inverseproblems/kersize_compute_dataloader.py
@@ -4,17 +4,17 @@ from tqdm import tqdm
 
 
 
-def kersize_samplingYX(distsXX, feasible_appartenance):
+def worstcase_kernelsize_appartenance(distsXX, feasible_appartenance):
     """
-    Computes the Kersize approximation as the maximum pairwise distance within feasible sets for all target samples.
+    Computes the worst-case kernelsize by computing the maximum pairwise distance of elements in feasible sets for all target samples.
+    The feasible sets are handed over pairwise indices in a feasible appartenance matrix.
 
     Args:
-        distsXX (scipy.sparse matrix): Pairwise distance matrix between target samples.
-        feasible_appartenance (torch.ndarray or sparse matrix): Feasibility matrix.
-        p_X (int or float): Norm degree for distance computation.
+        distsXX: (scipy.sparse matrix) containing pairwise distances between target samples.
+        feasible_appartenance: (torch.ndarray or sparse matrix) matric of allocation of pairwise data indices to feasible sets.
 
     Returns:
-        float: Kersize.
+        float: worst-case kernelsize.
     """
     def compute_max_distance(y_idx, fa, dXX):
         # Extract valid indices where feasible_appartenance[:, y_idx] is non-zero
@@ -40,21 +40,22 @@ def kersize_samplingYX(distsXX, feasible_appartenance):
 
     return np.nanmax(np.array(list(results)))
 
-def avgLB_samplingYX(distsXX, feasible_appartenance,p_X):
+def average_kernelsize_appartenance(distsXX, feasible_appartenance,p_X):
     """
-    Computes the lower bound of the average error.
+    Computes the average kernelsize from pairwise distances of elements in feasible sets for all target samples.
+    The feasible sets are handed over pairwise indices in a feasible appartenance matrix.
 
     Args:
-        distsXX (scipy.sparse matrix): Pairwise distance matrix between target samples (x).
-        feasible_appartenance (torch.ndarray or sparse matrix): Feasibility matrix.
+        distsXX: (scipy.sparse matrix) containing pairwise distances between target samples.
+        feasible_appartenance: (torch.ndarray or sparse matrix) matric of allocation of pairwise data indices to feasible sets.
         p_X (int or float): Norm degree for distance computation.
 
     Returns:
-        float: Average lower bound of feasible pairwise distances.
+        float: average kernelsize.
     """
-    def compute_mean_distance(y_idx, fa, dXX):
+    def compute_mean_distance(y_idx, feasible_appertinance_matrix, dXX):
         # Extract valid indices where feasible_appartenance[:, y_idx] is non-zero
-        valid_idx = fa[:, y_idx].nonzero()[0]
+        valid_idx = feasible_appertinance_matrix[:, y_idx].nonzero()[0]
         
         if len(valid_idx) == 0:
             return 0
@@ -64,12 +65,14 @@ def avgLB_samplingYX(distsXX, feasible_appartenance,p_X):
         subdistXX = subdistXX.toarray()  # Convert to dense matrix for max computation
         
         size_feas = len(valid_idx)
+        # compute average over sums of differences over twice the feasible sets 
         return np.divide(np.nansum(np.power(subdistXX,p_X)), np.power(size_feas,2))
 
 
     n,p = feasible_appartenance.shape
     results = Parallel(n_jobs=-1)(delayed(compute_mean_distance)(y_idx, feasible_appartenance, distsXX) for y_idx in tqdm(range(p)))
 
+    # get average over K input data samples (y) 
     return np.power(np.nanmean(np.asarray(list(results))),np.divide(1,p_X))
 
 
